8dbf721d7720703249133c858609c3a21d22b28f
Prince Changani <princechangani56385638@gmail.com>
2023-07-27 17:36:09 +0530

    setting router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
@@ -56,21 +56,19 @@ function App() {
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/>  */}
       {/* <Navbar /> */}
 
-      {/* <Router> */}
+      <Router>
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          {/* <Routes>
+          <Routes>
             <Route exact path="/about" element={<About />} />
             <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
             <Route path="*" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}/>
-          </Routes> */}
-
-          <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+          </Routes>
         </div>
-      {/* </Router> */}
+      </Router>
     </>
   );
 }
