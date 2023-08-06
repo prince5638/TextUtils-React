65671eb390e3f9e1b3b5bb7cc8aff215cb039a12
Prince Changani <princechangani56385638@gmail.com>
2023-08-06 10:00:03 +0530

    About component edited

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
             renders the first one that matches the current URL. */}
           <Routes>
             <Route exact path="/about" element={<About mode={mode} />} />
-            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" mode={mode} />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter  , Character Counter, Remove extra spaces" mode={mode} />} />
             <Route path="*" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}/>
           </Routes>
         </div>
--- src/components/About.js
+++ src/components/About.js
@@ -9,47 +9,47 @@ export default function About(props) {
     //     });
 
     let mystyle = {
-        color: props.mode === 'dark' ? 'white': '#042743', 
-        backgroundColor: props.mode === 'dark' ? 'rgb(36,74,104)': 'white' 
+        color: props.mode === 'dark' ? 'white' : '#042743',
+        backgroundColor: props.mode === 'dark' ? 'rgb(36,74,104)' : 'white'
     }
 
     return (
-        <div className='container my-2 py-1' style={{color: props.mode === 'dark' ? 'white': '#042743'}}>
-            <h1>About us</h1>
+        <div className='container my-2 py-1' style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+            <h1>Know More About TextUtils</h1>
             <div className="accordion" id="accordionExample">
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
                         <button className="accordion-button" style={mystyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Accordion Item #1
+                            <strong>1) Versatile Text Manipulation for React Web Pages</strong>
                         </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                         <div className="accordion-body" style={mystyle}>
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            TextUtils is a dynamic React-based web application designed to provide users with a powerful set of text manipulation tools. Whether you're a writer, student, professional, or simply someone who deals with text regularly, TextUtils simplifies various tasks related to text processing. From counting words and characters to converting text cases, it offers a range of features that streamline text-related operations.
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
                         <button className="accordion-button collapsed" style={mystyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Accordion Item #2
+                            <strong>2) Efficient and User-Friendly Features</strong>
                         </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                         <div className="accordion-body" style={mystyle}>
-                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            TextUtils offers a suite of user-friendly features that enhance productivity and convenience. Users can effortlessly count the number of words and characters in a given text, helping writers meet specific word count requirements. The application's case conversion functionality allows users to swiftly transform text between uppercase, lowercase, title case, and more. Additionally, TextUtils provides an estimated reading time for a piece of text, aiding in gauging the time needed to comprehend or present the content. The text copying feature simplifies the process of copying manipulated text, while the extra space removal tool ensures clean and consistent formatting.
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
                         <button className="accordion-button collapsed" style={mystyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Accordion Item #3
+                            <strong>3) Simplifying Text-Related Tasks with Ease</strong>
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                         <div className="accordion-body" style={mystyle}>
-                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            TextUtils is a go-to tool for anyone looking to streamline their text-related tasks. Whether it's preparing written content, optimizing text for better readability, or ensuring consistency in formatting, this application covers a wide array of use cases. By offering an intuitive and responsive user interface, TextUtils ensures that users can access its features seamlessly across different devices and platforms. With its focus on enhancing efficiency and saving time, TextUtils empowers users to accomplish their text manipulation goals effortlessly.
                         </div>
                     </div>
                 </div>
